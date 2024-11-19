bb18e0e00fdd533562dd283b7b86f1aa339c4545
Joao Gabriel Chiarello <joao.chiarello@unochapeco.edu.br>
2024-11-19 16:09:08 -0300

    Arrumado tela, novo componente e alguns estilos comuns

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,12 +1,58 @@
-import React, {Component} from "react";
-import { View, Text } from "react-native";
+import React, { Component } from "react";
+import { View, Text, ImageBackground, StyleSheet } from "react-native";
+import todayImage from '../../assets/imgs/today.jpg'
+import moment from "moment";
+import 'moment/locale/pt-br'
+import commonStyles from "../commonStyles";
+import Task from "../components/Task";
 
 export default class TaskList extends Component {
     render() {
+
+        const today = moment().locale('pt-br').format('ddd, D, [de] MMMM')
         return (
-            <View>
-                <Text>TaskList</Text>
+            <View style={styles.container}>
+                <ImageBackground source={todayImage} style={styles.backgroud}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subtitle}>{today}</Text>
+                    </View>
+                </ImageBackground>
+                <View style={styles.taskList}>
+                    <Task desc="Tarefa 1" estimateAt={new Date()} doneAt={new Date()}/>
+                    <Task desc="Tarefa 2" estimateAt={new Date()} doneAt={new Date()}/>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    backgroud: {
+        flex: 3
+    },
+    taskList: {
+        flex: 7
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    title: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 50,
+        color: commonStyles.colors.secundary,
+        marginLeft: 20,
+        marginBottom: 20,
+    },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30,
+        color: commonStyles.colors.secundary
+    }
+})
