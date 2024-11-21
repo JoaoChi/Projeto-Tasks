bfd7ac8aa40d50ade85e7410d288aa747ab5fd51
Joao Gabriel Chiarello <joao.chiarello@unochapeco.edu.br>
2024-11-21 18:15:32 -0300

    ultimas estilizações

--- src/screens/AddTask.js
+++ src/screens/AddTask.js
@@ -1,5 +1,9 @@
 import Raeact, { Component } from "react";
-import { View, Modal, StyleSheet, TouchableWithoutFeedback, Text } from "react-native";
+import { View, 
+    Modal, 
+    StyleSheet, 
+    TouchableWithoutFeedback, 
+    Text, TouchableOpacity, TextInput } from "react-native";
 import commonStyles from "../commonStyles";
 
 export default class AddTask extends Component {
@@ -7,21 +11,29 @@ export default class AddTask extends Component {
     render() {
         return (
             <Modal transparent={true} visible={this.props.isVisible}
-            onRequestClose={this.props.onCancel}
-            animationType="slide">
+                onRequestClose={this.props.onCancel}
+                animationType="slide">
                 <TouchableWithoutFeedback
                     onPress={this.props.onCancel}>
                     <View style={styles.background}>
                     </View>
                 </TouchableWithoutFeedback>
                 <View style={styles.container}>
-                <Text style={styles.header}>Nova Tarefa</Text>
-
+                    <Text style={styles.header}>Nova Tarefa</Text>
+                    <TextInput style={styles.input}/>
+                    <View style={styles.buttons}>
+                        <TouchableOpacity>
+                                <Text style={styles.button}>Cancelar</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                                <Text style={styles.button}>Salvar</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
                 <TouchableWithoutFeedback
                     onPress={this.props.onCancel}>
                     <View style={styles.background}>
-                        
+                         
                     </View>
                 </TouchableWithoutFeedback>
             </Modal>
@@ -45,5 +57,24 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         padding: 15,
         fontSize: 18,
+    },
+    input: {
+        fontFamily: commonStyles.fontFamily,
+        height: 40,
+        margin: 15,
+        backgroundColor: '#FFF',
+        borderWidth: 2,
+        borderColor: '#E3E3E3',
+        borderRadius: 6
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+
+    },
+    button: {
+        margin: 20,
+        marginRight: 3,
+        color: commonStyles.colors.today
     }
 })
